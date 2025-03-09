baa102d243577a161626a12c21ce94e1aff509e1
dauaneneves <dauanenevesgeron@gmail.com>
2025-03-09 16:11:10 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { RetanguloOO } from "./exercicio 1/main";
+import { ToDoList } from "./exercicio 2/main";
+import { BancoOO } from "./exercicio 3/main";
+import { GerenciadorTarefasPessoal } from "./exercicio 4/main";
+import { CalculadoraFormas } from "./exercicio 5/main";
+import { CadastroProdutos } from "./exercicio 6/main";
+
+const retangulo = new RetanguloOO(10, 5);
+retangulo.exibirDetalhes();
+
+const gerenciadorTarefas = new ToDoList();
+gerenciadorTarefas.adicionarTarefa("Estudar TypeScript");
+gerenciadorTarefas.listarTarefas();
+
+globalThis.console.log("\nBanco:");
+const banco = new BancoOO();
+banco.criarConta(123, 1000);
+banco.depositar(123, 500);
+banco.sacar(123, 200);
+banco.consultarSaldo(123);
+
+const tarefasPessoais = new GerenciadorTarefasPessoal();
+tarefasPessoais.adicionarTarefa("Fazer compras");
+tarefasPessoais.marcarComoConcluida(0);
+tarefasPessoais.exibirTarefas();
+
+const calculadora = new CalculadoraFormas();
+calculadora.testar();
+
+const cadastroProdutos = new CadastroProdutos();
+cadastroProdutos.adicionarProduto("Celular", 1500, 10);
+cadastroProdutos.atualizarPreco("Celular", 1800);
+cadastroProdutos.exibirRelatorio();
